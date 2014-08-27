4e6e42f6fba22da8c72124f2c3afb451ba3550df
Vitaly Berg <bergusman@gmail.com>
2014-08-27 10:43:23 +0400

    Initial Commit

---
+++ MegaFonTV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MegaFonTV
+//
+//  Created by Vitaly Berg on 27/08/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
