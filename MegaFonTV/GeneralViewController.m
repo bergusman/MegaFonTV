8335ac2082a7474c84a843e9824584110dc26f78
Vitaly Berg <bergusman@gmail.com>
2014-08-29 00:49:14 +0400

    Главный экран

--- MegaFonTV/GeneralViewController.m
+++ MegaFonTV/GeneralViewController.m
@@ -8,6 +8,8 @@
 
 #import "GeneralViewController.h"
 
+#import "DetailsViewController.h"
+
 @interface GeneralViewController ()
 
 @end
@@ -16,12 +18,35 @@
 
 #pragma mark - Setups
 
+- (void)setupNavigationItem {
+    self.navigationItem.title = @"МегаФон.ТВ";
+}
+
 #pragma mark - Content
+
+- (void)showDetails {
+    DetailsViewController *detailsVC = [[DetailsViewController alloc] init];
+    detailsVC.modalPresentationCapturesStatusBarAppearance = YES;
+    detailsVC.modalPresentationStyle = UIModalPresentationCustom;
+    detailsVC.transitioningDelegate = detailsVC;
+    [self presentViewController:detailsVC animated:YES completion:nil];
+}
+
+#pragma mark - Actions
+
+- (IBAction)openAction:(id)sender {
+    [self showDetails];
+}
+
+- (IBAction)tapAction:(id)sender {
+    [self showDetails];
+}
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupNavigationItem];
 }
 
 @end
